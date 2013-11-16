fee93dec015dd51cee7b7030760b98e6172558a3
Patrick Liang <patrick.liang.7@gmail.com>
2013-11-15 22:26:09 -0800

    Posts the cart JSON with alert

--- js/controller.js
+++ js/controller.js
@@ -29,5 +29,40 @@ $(function(){
 
 	//refresh to get movies from server
 	moviesModel.refresh();
+
+	//when the movies view triggers 'addToCart'
+	//add a new item to the cart, using the supplied
+	//movieID and format
+	moviesView.on('addToCart', function(data){
+	    var movie = moviesModel.getItem(data.movieID);
+	    if (!movie)
+	        throw 'Invalid movie ID "' + movieID + '"!'; 
+
+	    cartModel.addItem({
+	        id: movie.id,
+	        title: movie.title,
+	        format: data.format,
+	        formatLabel: formatLabels[data.format],
+	        price: movie.prices[data.format]
+	    });
+	}); //addToCart event
+
+	$('.place-order').click(function() {
+		$.ajax({
+		    url: 'https://courses.washington.edu/info343/ajax/movies/orders/',
+		    type: 'POST',
+		    data: cartModel.toJSON(),
+		    contentType: 'application/json',
+		    success: function(responseData) {
+		        //code to run after successful post
+		        alert(responseData.message);
+		        cartModel.setItems([]);
+		    },
+		    error: function(jqXHR, status, errorThrown) {
+		        //error with post--alert user
+		        alert(errorThrown || status);
+		    }
+		}); //ajax()
+	});
 }); //doc ready()
 
